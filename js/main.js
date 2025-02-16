0417507da6486de59aee3701e795c2997cbca3f6
aro-student <refreshedagains@yandex.ru>
2025-02-16 04:56:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const age = 32
+const firstName = 'alex'
+const lastName = 'gerome'
+const height = 172
+const isStudent = true
+
+console.log(age, firstName, lastName, height, isStudent)
+
+/* ================================================ */
+
+function myName() {
+    console.log(`hello ${firstName}`)
+}
+myName()
+
+/* ================================================ */
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function getNumber() {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 10) {
+            console.log(`The number is ${array[i]}`);
+        }
+    }
+}
+
+getNumber();
+
+/* ================================================ */
+
+function calc(num1, num2, operator) {
+    if (operator === "minus") {
+        console.log(num1 - num2)
+    } else if (operator === "plus") {
+        console.log(num1 + num2)
+    } else if (operator === "divide") {
+        console.log(num1 / num2)
+    } else if (operator === "multiply") {
+        console.log(num1 * num2)
+    }
+}
+calc(5, 5, "multiply")
+
+/* ================================================ */
+
+const users = [
+    { name: 'alex', age: 21, isAdmin: false, country: 'USA' },
+    { name: 'john', age: 22, isAdmin: true, country: 'UK' },
+    { name: 'mary', age: 25, isAdmin: false, country: 'Canada' },
+    { name: 'susan', age: 28, isAdmin: true, country: 'Australia' },
+    { name: 'david', age: 30, isAdmin: false, country: 'Germany' },
+    { name: 'lisa', age: 27, isAdmin: false, country: 'France' },
+    { name: 'mike', age: 29, isAdmin: true, country: 'Italy' }
+];
+
+let regularUser = 0;
+
+for (let i = 0; i < users.length; i++) {
+    if (users[i].isAdmin == false) {
+        regularUser = regularUser + 1;
+    }
+};
+console.log(regularUser)
