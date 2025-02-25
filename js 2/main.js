5ef173f651122f973d088039b4dfa01f2d889609
aro-student <refreshedagains@yandex.ru>
2025-02-26 02:15:52 +0300

    Add files via upload

---
+++ js 2/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const btn = document.querySelector('.btn-open')
+const modal = document.querySelector('.modal')
+
+
+btn.addEventListener('click', () => {
+    modal.classList.add('modal--open')
+})
+
+modal.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
+        modal.classList.remove('modal--open')
+    }
+})
